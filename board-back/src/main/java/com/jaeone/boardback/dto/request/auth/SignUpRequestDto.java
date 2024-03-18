fa1ed01a923a280e6d4ec2169989e15a7e2e205c
YangJaeWon <yang7458@naver.com>
2024-03-18 21:36:56 +0900

    회원가입 기능 구현 (DTO 작성)

---
+++ board-back/src/main/java/com/jaeone/boardback/dto/request/auth/SignUpRequestDto.java
@@ -0,0 +1,35 @@
+package com.jaeone.boardback.dto.request.auth;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class SignUpRequestDto {
+
+    @NotBlank @Email
+    private String email;
+
+    @NotBlank @Size(min = 8, max = 20)
+    private String password;
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank @Pattern(regexp = "^[0-9]{11,13}$")
+    private String telNumber;
+
+    @NotBlank
+    private String address;
+
+    private String addressDetail;
+
+    @NotNull @AssertTrue
+    private Boolean agreedPersonal;
+}
