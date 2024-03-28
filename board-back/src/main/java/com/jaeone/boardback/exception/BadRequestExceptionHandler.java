3c4d8dfdc1134b9f2dc2062426a23164f39beed6
YangJaeWon <yang7458@naver.com>
2024-03-28 22:48:47 +0900

    Validation Failed 처리

---
+++ board-back/src/main/java/com/jaeone/boardback/exception/BadRequestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.jaeone.boardback.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.jaeone.boardback.dto.response.ResponseDto;
+
+@RestControllerAdvice
+public class BadRequestExceptionHandler {
+    
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<ResponseDto> validationExceptionHandler(Exception e) {
+        return ResponseDto.validationFailed();
+    }
+}
